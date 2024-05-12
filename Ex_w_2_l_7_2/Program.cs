d501344ebeadc582fb1856c71a36171b490d1f2c
Andrzej Kowalski <jedreksmith@poczta.onet.pl>
2024-05-12 22:03:33 +0200

    Add project files.

---
+++ Ex_w_2_l_7_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.Design;
+
+namespace W_2_l_7_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Podaj liczbe");
+            a = int.Parse(Console.ReadLine());
+
+            if ((a % 2) == 0) {
+                Console.WriteLine(a + " Liczba parzysta");
+            }
+            else
+            { Console.WriteLine(a + " Liczba nieparzysta"); }
+        }
+    }
+}
